d3b4c1615431807bdb3664cb32f32fc935ae28e4
Ashumehra842 <ashumehra768@outlook.com>
2025-08-26 15:26:58 +0530

    push latest

--- index.js
+++ index.js
@@ -26,12 +26,12 @@ if any unknown route hit from url the it will show custom error
 */
 app.all(/.*/, (req, res, next) => {
 	// const err = new Error(`The Url: ${req.originalUrl} trying to access is not found.`);
-	
+
 	/*Below is optimized way to handle error*/
 	next(new AppError(`The Url: ${req.originalUrl} trying to access is not found.`, 404));
 });
 
-
+ 
 /*Bellow is the express middleware to handle the error globally */
 
 app.use(ErrorHandlerController);
