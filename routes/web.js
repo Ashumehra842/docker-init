1aa41fed5fd164bc12ecf861b3b2ba5d37f98ab5
AshishMehra <riyanshmehra76021@gmail.com>
2025-10-21 21:10:50 +0530

    worked onn the password forgot and password reset functionality

--- routes/web.js
+++ routes/web.js
@@ -1,21 +1,27 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const dashboard = require('../Controllers/IndexController');
-const authController = require('./../Controllers/authController');
-const upload = require('./../utils/upload');
+const dashboard = require("../Controllers/IndexController");
+const authController = require("./../Controllers/authController");
+const upload = require("./../utils/upload");
 
-
-
-
-router.route('/dashborad',).get(dashboard.index);
-router.route('/save').post(dashboard.saveUser);
-router.route('/list').get(authController.protect, dashboard.getAllUsers);
-router.route('/getUserById/:id').get(dashboard.getUserById);
-router.route('/update/:id').post(dashboard.updateUser);
-router.route('/dashborad').get(dashboard.index);
-router.route('/profile').post(upload.single("image"), dashboard.profile);
-router.post('/delete/:id', authController.protect, authController.restrictedTo('admin'), authController.deleteUser);
+router.route("/dashborad").get(dashboard.index);
+router.route("/save").post(dashboard.saveUser);
+router.route("/list").get(authController.protect, dashboard.getAllUsers);
+router.route("/getUserById/:id").get(dashboard.getUserById);
+router.route("/update/:id").post(dashboard.updateUser);
+router.route("/dashborad").get(dashboard.index);
+router.route("/profile").post(upload.single("image"), dashboard.profile);
+router.post(
+  "/delete/:id",
+  authController.protect,
+  authController.restrictedTo("admin"),
+  authController.deleteUser
+);
 /** Auth Controller routes */
-router.post('/signup', authController.signup);
-router.post('/login', authController.login);
+router.post("/signup", authController.signup);
+router.post("/login", authController.login);
+
+router.post("/forgotPassword", authController.forgotPassword);
+
+router.patch("/resetPassword/:token", authController.resetPassword);
 module.exports = router;
